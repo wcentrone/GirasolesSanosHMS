b2727fe3390dc92962894c2f05f84f7159cbcdb1
Wayne Centrone <waynecentrone@MacBook-Pro-2.local>
2026-03-20 09:58:04 -0700

    Disable TS strict checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 "use client";
 import { useState, useEffect, useRef } from "react";
 import { LineChart, Line, BarChart, Bar, RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart, Pie, Cell, AreaChart, Area } from "recharts";
